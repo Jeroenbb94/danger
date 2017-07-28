613a5956aca980ec46f437ae476b7d188353c0e8
Jeroen Bakker <j.bakker@wearetriple.com>
2017-07-28 09:35:01 +0200

    debug system_env

--- lib/danger/danger_core/executor.rb
+++ lib/danger/danger_core/executor.rb
@@ -59,7 +59,7 @@ module Danger
     def validate_pr!(cork)
       unless EnvironmentManager.pr?(system_env)
         # cork.puts "Not a Pull Request - skipping `danger` run".yellow
-        cork.puts cork.to_yaml
+        cork.puts system_env
         exit(0)
       end
     end
